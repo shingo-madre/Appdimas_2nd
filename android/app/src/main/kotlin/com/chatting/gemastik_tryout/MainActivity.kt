69826212a7bbd63f69b11c1dea75ada15014f11f
shingo-madre <rakazein.akbar@gmail.com>
2021-06-23 19:14:09 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/chatting/gemastik_tryout/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chatting.gemastik_tryout
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main () {
+  runApp(null);
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
