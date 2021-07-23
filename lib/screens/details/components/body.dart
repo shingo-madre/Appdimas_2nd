f6b765b99d5e70764d71e0b239cfbd870c8865dc
Dafa Raisya <dafaraisya.dr@gmail.com>
2021-07-23 23:03:41 +0700

    add event detail

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -34,7 +34,51 @@ class Body extends StatelessWidget {
                 EventDescription(
                   event: event,
                 ),
-                EventTabLayout()
+                SizedBox(
+                  height: 20,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Text('Detail',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold)
+                      ),
+                  ),
+                ),
+                Divider(
+                  height: 20,
+                  thickness: 5,
+                  indent: 20,
+                  endIndent: 320,
+                  color: Colors.orange,
+                ),
+                ListTile(
+                  leading: Icon(Icons.location_on, color: Colors.orange),
+                  title: Text(
+                    'Gelora Sepuluh Nopember',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.timer,
+                    color: Colors.orange,
+                  ),
+                  title: Text('3 Juli - 4 Juli', style: TextStyle(fontSize: 14)),
+                ),
+                ListTile(
+                  leading: Icon(Icons.bookmark, color: Colors.orange),
+                  title: Text('Menjaga Stan Vaksinasi', style: TextStyle(fontSize: 14)),
+                ),
+                ListTile(
+                  leading: Icon(Icons.check_box, color: Colors.orange),
+                  title: Text('Mencatat Kondisi Kesehatan \nMengecek Tekanan Darah', style: TextStyle(fontSize: 14)),
+                ),
+                // EventTabLayout()
               ],
             ),
           ),
@@ -42,18 +86,18 @@ class Body extends StatelessWidget {
       ),
       bottomNavigationBar: EventDetailNavBar(),
     );
-      // bottomNavigationBar: BottomNavigationBar(
-      //   items: <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.chat_rounded),
-      //       label: 'Home',
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.business),
-      //       label: 'Business',
-      //     ),
-      //   ],
-      //   backgroundColor: Colors.white,
-      // ),
+    // bottomNavigationBar: BottomNavigationBar(
+    //   items: <BottomNavigationBarItem>[
+    //     BottomNavigationBarItem(
+    //       icon: Icon(Icons.chat_rounded),
+    //       label: 'Home',
+    //     ),
+    //     BottomNavigationBarItem(
+    //       icon: Icon(Icons.business),
+    //       label: 'Business',
+    //     ),
+    //   ],
+    //   backgroundColor: Colors.white,
+    // ),
   }
 }
--- lib/screens/details/components/event_tab_layout.dart
+++ lib/screens/details/components/event_tab_layout.dart
@@ -12,59 +12,83 @@ class EventTabLayout extends StatefulWidget {
 class _EventTabLayoutState extends State<EventTabLayout> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: DefaultTabController(
-        length: 4,
-        child: Column(
-          children: <Widget>[
-            Container(
-              constraints: BoxConstraints(maxHeight: 150.0),
-              child: Material(
-                color: Colors.white,
-                child: TabBar(
-                  indicatorColor: Colors.deepOrange,
-                  tabs: [
-                    Tab(
-                      child: Text(
-                        'Semua',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    Tab(
-                      child: Text(
-                        'Terbaru',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    Tab(
-                      child: Text(
-                        'Populer',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                    Tab(
-                      child: Text(
-                        'Terdekat',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
             ),
-            Expanded(
-              child: TabBarView(
-                children: [
-                  ListEventAll(),
-                  ListEventAll(),
-                  ListEventAll(),
-                  PopularProducts(),
-                ],
-              ),
-            ),
-          ],
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
+          ),
         ),
       ),
     );
+    // return MaterialApp(
+    //   home: DefaultTabController(
+    //     length: 4,
+    //     child: Column(
+    //       children: <Widget>[
+    //         Container(
+    //           constraints: BoxConstraints(maxHeight: 150.0),
+    //           child: Material(
+    //             color: Colors.white,
+    //             child: TabBar(
+    //               indicatorColor: Colors.deepOrange,
+    //               tabs: [
+    //                 Tab(
+    //                   child: Text(
+    //                     'Semua',
+    //                     style: TextStyle(color: Colors.black),
+    //                   ),
+    //                 ),
+    //                 Tab(
+    //                   child: Text(
+    //                     'Terbaru',
+    //                     style: TextStyle(color: Colors.black),
+    //                   ),
+    //                 ),
+    //                 Tab(
+    //                   child: Text(
+    //                     'Populer',
+    //                     style: TextStyle(color: Colors.black),
+    //                   ),
+    //                 ),
+    //                 Tab(
+    //                   child: Text(
+    //                     'Terdekat',
+    //                     style: TextStyle(color: Colors.black),
+    //                   ),
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //         ),
+    //         Expanded(
+    //           child: TabBarView(
+    //             children: [
+    //               ListEventAll(),
+    //               ListEventAll(),
+    //               ListEventAll(),
+    //               PopularProducts(),
+    //             ],
+    //           ),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
